ba6c09c2628dc60ef2d33baae8d85257163c6b13
SembiaStudios <91810880+SembiaStudios@users.noreply.github.com>
2023-05-13 20:41:45 +0200

    firebase

--- site/script.js
+++ site/script.js
@@ -1,53 +1,164 @@
-var questions = [
-    { question: "Was bedeutet 'Platzhalter' auf Englisch?", choices: ["Placeholder", "AB", "A", "EF"], correctAnswer: 0 },
-    { question: "Was bedeutet 'Platzhalter' auf Englisch?", choices: ["Dg", "Placeholder", "Ho", "Te"], correctAnswer: 1 },
-    { question: "Was bedeutet 'Platzhalter' auf Englisch?", choices: ["Do", "Ct", "Placeholder", "Tr"], correctAnswer: 2 },
-    // Weitere Fragen hinzufügen...
-];
+// Firebase-Konfiguration
+var firebaseConfig = {
+  apiKey: "AIzaSyD6h0dpMnNF2_QN0Vl4k0ipou13O4lV-fQ",
+  authDomain: "languagelab---quiz.firebaseapp.com",
+  projectId: "languagelab---quiz",
+  appId: "1:478036064155:web:b22feda4d0cb526bf3133e"
+};
 
-var currentQuestionIndex = 0;
-var score = 0;
+// Firebase initialisieren
+firebase.initializeApp(firebaseConfig);
+var auth = firebase.auth();
+var db = firebase.firestore();
 
+// DOM-Elemente
+var loginContainer = document.getElementById("login-container");
+var quizContainer = document.getElementById("quiz-container");
+var scoreboardContainer = document.getElementById("scoreboard-container");
+var emailInput = document.getElementById("email");
+var passwordInput = document.getElementById("password");
+var errorMessage = document.getElementById("error-message");
+var questionElement = document.getElementById("question");
+var choiceElements = document.querySelectorAll("#choices button");
+var scoreElement = document.getElementById("score");
+var scoreboardElement = document.getElementById("scoreboard");
+
+// Benutzer anmelden
+function login() {
+  var email = emailInput.value;
+  var password = passwordInput.value;
+
+  auth.signInWithEmailAndPassword(email, password)
+    .then(function() {
+      showQuiz();
+    })
+    .catch(function(error) {
+      errorMessage.textContent = error.message;
+    });
+}
+
+// Google-Anmeldung
+function loginWithGoogle() {
+    var provider = new firebase.auth.GoogleAuthProvider();
+
+    auth.signInWithPopup(provider)
+    .then(function(result) {
+        showQuiz();
+    })
+    .catch(function(error) {
+        errorMessage.textContent = error.message;
+    });
+}
+
+
+// Benutzer abmelden
+function logout() {
+  auth.signOut()
+    .then(function() {
+      showLogin();
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+}
+
+// Quiz anzeigen
+function showQuiz() {
+  loginContainer.style.display = "none";
+  scoreboardContainer.style.display = "none";
+  quizContainer.style.display = "block";
+  scoreElement.textContent = "Punktzahl: 0";
+
+  showQuestion();
+}
+
+// Frage anzeigen
 function showQuestion() {
-    var questionElement = document.getElementById("question");
-    var choiceElements = document.querySelectorAll("#choices button");
-    var currentQuestion = questions[currentQuestionIndex];
+  var currentQuestion = questions[currentQuestionIndex];
 
-    questionElement.textContent = currentQuestion.question;
+  questionElement.textContent = currentQuestion.question;
 
-    for (var i = 0; i < choiceElements.length; i++) {
-        choiceElements[i].textContent = currentQuestion.choices[i];
-    }
+  for (var i = 0; i < choiceElements.length; i++) {
+    choiceElements[i].textContent = currentQuestion.choices[i];
+  }
 }
 
+// Antwort überprüfen
 function checkAnswer(choiceIndex) {
-    var currentQuestion = questions[currentQuestionIndex];
+  var currentQuestion = questions[currentQuestionIndex];
 
-    if (choiceIndex === currentQuestion.correctAnswer) {
-        score++;
-        alert("Richtig!");
-    } else {
-        alert("Falsch!");
-    }
+  if (choiceIndex === currentQuestion.correctAnswer) {
+    score++;
+    alert("Richtig!");
+  } else {
+    alert("Falsch!");
+  }
 
-    currentQuestionIndex++;
+  currentQuestionIndex++;
 
-    if (currentQuestionIndex < questions.length) {
-        showQuestion();
-    } else {
-        endQuiz();
-    }
+  if (currentQuestionIndex < questions.length) {
+    showQuestion();
+  } else {
+    endQuiz();
+  }
 }
 
+// Quiz beenden
 function endQuiz() {
-    var questionContainer = document.getElementById("question-container");
-    var choicesContainer = document.getElementById("choices");
-    var scoreElement = document.getElementById("score");
+  var user = auth.currentUser;
 
-    questionContainer.style.display = "none";
-    choicesContainer.style.display = "none";
-    scoreElement.textContent = "Punktzahl: " + score;
-    scoreElement.style.display = "block";
+  quizContainer.style.display = "none";
+  scoreboardContainer.style.display = "block";
+
+  // Punktzahl in der Datenbank speichern
+  db.collection("scores").add({
+    user: user.uid,
+    score: score
+  })
+    .then(function() {
+      showScoreboard();
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
 }
 
-showQuestion();
+// Scoreboard anzeigen
+function showScoreboard() {
+  scoreboardElement.innerHTML = "";
+
+  db.collection("scores").orderBy("score", "desc").limit(10).get()
+    .then(function(querySnapshot) {
+      querySnapshot.forEach(function(doc) {
+        var scoreData = doc.data();
+        var li = document.createElement("li");
+        li.textContent = scoreData.score;
+        scoreboardElement.appendChild(li);
+      });
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+}
+
+// Benutzerstatus überprüfen
+auth.onAuthStateChanged(function(user) {
+  if (user) {
+    showQuiz();
+  } else {
+    showLogin();
+  }
+});
+
+// Anmeldeformular anzeigen
+function showLogin() {
+    loginContainer.style.display = "block";
+
+    quizContainer.style.display = "none";
+    scoreboardContainer.style.display = "none";
+    emailInput.value = "";
+    passwordInput.value = "";
+    errorMessage.textContent = "";
+}
+
+showLogin();
